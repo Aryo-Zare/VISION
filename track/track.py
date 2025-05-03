293627e86c8e33d9e87d2ebe59e41b0f036a7a31
Aryo Zare <aryozz@gmail.com>
2025-05-03 15:06:53 +0200

    Converted the units from pixel to meter _ prepared figures for the presentation.

--- track/track.py
+++ track/track.py
@@ -1,8 +1,14 @@
 
 
-# %%'
+# %%.
 
 point = pd.read_csv( r'U:\VISION\track\results-7\center_points.csv' )
+
+point_downsampled.to_csv( r'U:\VISION\track\results-7\point_downsampled.csv'  )
+
+point_downsampled = pd.read_csv( r'U:\VISION\track\results-7\point_downsampled.csv'  )
+
+# %%'
 
 point.shape
     # Out[7]: (449, 2)
@@ -15,28 +21,65 @@ point[:4]
     # 2  323  159
     # 3  322  159
 
+point_downsampled.columns 
+    # Out[10]: 
+    # Index(['Unnamed: 0', 'x', 'y', 'xm', 'ym', 'x_smooth', 'y_smooth', 'dx', 'dy',
+    #        'distance', 'distance_m', 'velocity_ms', 'time_s', 'acc_ms2'],
+    #       dtype='object')
+
 # %%'
 
 point['x'].min()
     # Out[19]: 315
 
+# %%'
+
+# 1000 pixels = 2.5 m
+
+# %% m _ xy
+
+# pixel to meter
+point['xm'] = point['x'] * 0.0025
+point['ym'] = point['y'] * 0.0025
+
 # %% Trajectory
 
-fig , ax = plt.subplots( figsize =(12,12) )
+# Trajectory
+# if loading 'point_downsampled' from the beginning, you should not use it instead of 'point' here.
+    # reason : it's downsampled & will result a low-resolution trajectory !
+        # we do not need downsampling for trajectory plotting !
 
-plt.plot( point.iloc[:,0] , point.iloc[:,1] )
-plt.title('Trajectory')
+fig , ax = plt.subplots( figsize =(10,12) )
+
+
+# the y coordinates of video-images are rendered from top to bottom.
+# hence, here, the y coordinates are mirrored (-) to conform to the video !!
+plt.plot( point['xm'] , -point['ym'] , linewidth=16 )
+
+# plt.plot( point.iloc[:,0] , point.iloc[:,1] )
+
+# the y coordinates of video-images are rendered from top to bottom.
+# hence, here, the y coordinates are mirrored (-) to conform to the video !!
+# plt.plot( point.iloc[:,0] , -point.iloc[:,1] )
+
+plt.title('Movement trajectory of a pig in a 15s test video')
+
+plt.xlabel( 'door side (m)'  , loc='right' )
+plt.ylabel( 'barrier side (m)' , loc='top' )
 
 fig.tight_layout()
 
-# %%'
+# %%%'
 
-plt.savefig( r'U:\VISION\track\results-7\1\Trajectory.pdf' )
+plt.savefig( r'U:\VISION\track\results-7\2\trajectory_2.pdf' )
+plt.savefig( r'U:\VISION\track\results-7\2\trajectory_3.svg' )
+
 
 # 250 - 800 : the span of x & y coordinates, according to the figure.
 
-# %%'
+# %% Emil's analysis. 
 
+# Emil's analysis.
 dva = pd.read_csv( r'U:\VISION\track\results-7\distance_velocity_acceleration.csv' )
 
 dva.shape
@@ -75,8 +118,10 @@ dva.iloc[28:32,0]
     # Name: time_s, dtype: float64
 
 # %%'
-# %%'
+# %% low-pass filter
 
+# filtering 
+# downsampling
 
 # Assume df is your DataFrame with 'time', 'x', and 'y' columns
 # df = df.sort_values('time')
@@ -109,16 +154,30 @@ point_downsampled[:4]
 # %%'
 
 # Step 3: Calculate differences for velocity estimation on downsampled data
-df_downsampled['dt'] = df_downsampled['time'].diff()
 
 point_downsampled['dx'] = point_downsampled['x_smooth'].diff()
 point_downsampled['dy'] = point_downsampled['y_smooth'].diff()
 
-# Distance and velocity calculation
+# Distance  
 point_downsampled['distance'] = np.sqrt( point_downsampled['dx']**2 + point_downsampled['dy']**2 )
+
+# conversion from pixel to meter.
+point_downsampled['distance_m'] = point_downsampled['distance'] * 0.025
+
+
+time_range = np.linspace( 0 , 15 , num=45 )
+time_range.shape
+    # Out[30]: (45,)
+time_range[:4]
+    # Out[28]: array([0.        , 0.34090909, 0.68181818, 1.02272727])
+time_range[-4:]
+    # Out[31]: array([13.97727273, 14.31818182, 14.65909091, 15.        ])
+
+point_downsampled['time_s'] = time_range
 
 # %%'
 
+# this is the distance the animal traveled per frame.
 point_downsampled['distance'][:5]
     # Out[16]: 
     # 0         NaN
@@ -128,7 +187,7 @@ point_downsampled['distance'][:5]
     # 4    4.841487
     # Name: distance, dtype: float64
 
-# %% speed
+# %% velocity _ pixel
 
 # I did not divide by time, as time is a fixed number & the units here are arbitrary.
 
@@ -148,14 +207,42 @@ plt.savefig( r'U:\VISION\track\results-7\1\velocity_3.pdf' )
 
 # %%'
 
-# not performed
-df_downsampled['velocity'] = df_downsampled['distance'] / df_downsampled['dt']
-df_downsampled['velocity'] = df_downsampled['velocity'].fillna(0)
+# velocity in m/s
+# the data is downsampled : each 'distance' datapoint corresponds to the distance traveled in 10 frames ( 1/3 s )
+point_downsampled['velocity_ms'] = point_downsampled['distance_m'] / ( 1/3 )
+
+# point_downsampled['velocity_ms'] = point_downsampled['distance_m'] / ( 1/3 )
+
+
+# not performed ( original from the interface ).
+# df_downsampled['velocity'] = df_downsampled['distance'] / df_downsampled['dt']
+# df_downsampled['velocity'] = df_downsampled['velocity'].fillna(0)
+
+# %% velocity _ m/s
+
+# velocity _ m/s
+
+fig , ax = plt.subplots( figsize =(15,12) )
+
+plt.plot( point_downsampled['time_s'] , point_downsampled['velocity_ms'] , linewidth=16 )
+plt.title('velocity of movement')
+
+plt.xlabel('time (s)' , loc='right')
+plt.ylabel('velocity (m/s)' , loc='top')
+
+fig.tight_layout()
+
+# %%'
+
+plt.savefig( r'U:\VISION\track\results-7\2\velocity_ms.pdf' )
 
 # %% acceleration 
 
 # again, as the denominator ( dt ) is equal among all rows, I discarded this.
 point_downsampled['acc'] = point_downsampled['distance'].diff()
+
+# acceleration in m/s^2
+point_downsampled['acc_ms2'] = point_downsampled['velocity_ms'].diff() / ( 1/3 )
 
 # %%%'
 
@@ -163,20 +250,113 @@ point_downsampled['acc'] = point_downsampled['distance'].diff()
 
 fig , ax = plt.subplots( figsize =(15,12) )
 
-plt.plot( point_downsampled['acc'] )
+plt.plot( point_downsampled['time_s'] , point_downsampled['acc_ms2'] , linewidth=16)
 
-plt.title('Acceleration ( arbitrary units )')
+plt.title('Acceleration')
 
-plt.xlabel('time' , loc='right')
-plt.ylabel('acceleration' , loc='top')
+plt.xlabel('time (s)' , loc='right')
+# The \mathrm{} ensures that "m/s" appears in upright text rather than italicized math mode.
+plt.ylabel( r'Acceleration $(\mathrm{m/s^2})$'  , loc='top')
+#Italicized
+# plt.ylabel( r'Acceleration $(m/s^2)$'  , loc='top')    
 
 fig.tight_layout()
 
 # %%'
 
-plt.savefig( r'U:\VISION\track\results-7\1\acceleration.pdf' )
+plt.savefig( r'U:\VISION\track\results-7\2\acceleration.pdf' )
+
+# %% presentation !
+# %%%'
+# %%%'
+
+fig , ax = plt.subplots( 2,1 , figsize=(8,12) , sharex=True )
+
+# %%%'
+
+ax[0].plot( point_downsampled['time_s'] , point_downsampled['velocity_ms'] , linewidth=14 )
+ax[0].set_title('Velocity')
+
+ax[0].set_xlabel('time (s)' , loc='right')
+ax[0].set_ylabel('Velocity (m/s)' , loc='top')
+
+# %%%'
+
+ax[1].plot( point_downsampled['time_s'] , point_downsampled['acc_ms2'] , linewidth=14)
+
+ax[1].set_title('Acceleration')
+
+ax[1].set_xlabel('time (s)' , loc='right')
+# The \mathrm{} ensures that "m/s" appears in upright text rather than italicized math mode.
+ax[1].set_ylabel( r'Acceleration $(\mathrm{m/s^2})$'  , loc='top')
+#Italicized
+# plt.ylabel( r'Acceleration $(m/s^2)$'  , loc='top')    
+
+# %%
+
+# fig.suptitle( 'Velocity & acceleration of movement' , fontsize=24 )
+
+fig.tight_layout()
+
+# %%
+
+# velocity & acceleration
+plt.savefig( r'U:\VISION\track\results-7\2\va.pdf' )
+plt.savefig( r'U:\VISION\track\results-7\2\va.svg' )
+
+
+# %% multi-plot
+# %%%'
+# %%%%'
+
+# the initial intention was to include 3 plots ( including the trajectory, which has different dimmensions than the other 2 plots ).
+
+# Create figure and specify GridSpec
+fig = plt.figure(figsize=(14, 12))
+gs = gridspec.GridSpec(2, 2, width_ratios=[1.1, 1], height_ratios=[1, 1])
+
+# %%%'
+
+# First plot (rectangle, spanning full y-axis)
+ax1 = plt.subplot(gs[:, 0])  # Spans both rows in the first column
+ax1.plot( point['xm'] , -point['ym'] , linewidth=16 )
+
+# plt.plot( point.iloc[:,0] , point.iloc[:,1] )
+
+# the y coordinates of video-images are rendered from top to bottom.
+# hence, here, the y coordinates are mirrored (-) to conform to the video !!
+# plt.plot( point.iloc[:,0] , -point.iloc[:,1] )
+
+ax1.set_title('Movement trajectory of a pig in a 15s test video')
+
+ax1.set_xlabel( 'door side (m)'  , loc='right' )
+ax1.set_ylabel( 'barrier side (m)' , loc='top' )
+
+# %%%'
+
+
+ax1.set_title("Rectangle-shaped plot")
+
+# Second plot (square, top half of right column)
+ax2 = plt.subplot(gs[0, 1])  # Occupies top half of right column
+ax2.set_title("Square-shaped plot 1")
+
+# %%%'
+
+# Third plot (square, bottom half of right column)
+ax3 = plt.subplot(gs[1, 1])  # Occupies bottom half of right column
+ax3.set_title("Square-shaped plot 2")
+
+# %%%'
+
+
+# Show plot
+plt.tight_layout()
 
 # %%'
+
+
+
 
 
 
